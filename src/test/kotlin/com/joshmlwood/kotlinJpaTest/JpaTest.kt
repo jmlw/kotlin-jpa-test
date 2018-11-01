2d64363c3f2e06b01a6fbac6378aada08b7a83ca
Josh Wood <jwood@carrierdirect.com>
2018-11-01 11:16:18 -0500

    transactional method which modifies nested data results in unsupported operation exception

--- src/test/kotlin/com/joshmlwood/kotlinJpaTest/JpaTest.kt
+++ src/test/kotlin/com/joshmlwood/kotlinJpaTest/JpaTest.kt
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.transaction.annotation.Transactional
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -18,6 +19,18 @@ class JpaTest {
 
     @Test
     fun `save wrapper then modify nested and save again`() {
+        val nested = Nested(null, "data")
+        val wrapper = Wrapper(null, "data", listOf(nested))
+        val saved = wrapperRepository!!.save(wrapper)
+        val nestedNew = Nested(saved.nested!![0].id, "someNewData")
+        val wrapperNew = Wrapper(saved.id, saved.data, listOf(nestedNew))
+        val saved2 = wrapperRepository!!.save(wrapperNew)
+        println(saved2)
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    @Transactional
+    fun `save wrapper then modify nested and save again expect exception`() {
         val nested = Nested(null, "data")
         val wrapper = Wrapper(null, "data", listOf(nested))
         val saved = wrapperRepository!!.save(wrapper)
